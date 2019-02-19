790fc7e25101ca46d09800ae951666fb2d3ac9a8
Dmitry Demidovsky <d.demidovsky@gmail.com>
2019-02-19 16:20:18 +0300

    Basic server

---
+++ index.js
@@ -0,0 +1,16 @@
+const finalhandler = require('finalhandler')
+const http = require('http')
+const serveStatic = require('serve-static')
+const PORT = 3000;
+
+// Serve up public folder
+const serve = serveStatic('public', {'index': ['index.html', 'index.htm']})
+
+// Create server
+const server = http.createServer(function onRequest (req, res) {
+  serve(req, res, finalhandler(req, res))
+})
+
+// Listen
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+
